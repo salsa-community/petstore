141bce5288e57298baa7534c2792a1ac53aef55f
danimaniarqsoft <daniel.cortes.pichardo@gmail.com>
2024-09-12 13:29:23 -0600

    12/07/2024 Registro de componente en el menú

--- gateway/src/main/webapp/app/entities/entities-menu.vue
+++ gateway/src/main/webapp/app/entities/entities-menu.vue
@@ -8,6 +8,10 @@
       <font-awesome-icon icon="asterisk" />
       <span v-text="t$('global.menu.entities.alimentomsAlimento')"></span>
     </b-dropdown-item>
+    <b-dropdown-item to="/tareas">
+      <font-awesome-icon icon="asterisk" />
+      <span v-text="t$('global.menu.entities.task')"> </span>
+    </b-dropdown-item>
     <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
   </div>
 </template>
